8c47b76131fa71ceb6782d2ed2c64413eee1026b
Wayne Duran <asartalo@gmail.com>
2016-11-01 06:51:28 +0800

    Refactoring steps

---
+++ features/step_definitions/authentication_steps.rb
@@ -0,0 +1,25 @@
+Given(/^I am not logged in$/) do
+end
+
+Given(/^I don't have a google account$/) do
+  OmniAuth.config.mock_auth[:default] = :no_account
+end
+
+Given(/^I have a google account$/) do
+  OmniAuth.config.mock_auth[:default] = {
+   "provider"=>"google_oauth2",
+   "uid"=>"100299566497892936264",
+   "info"=>
+   {"name"=>"john.doe@gmail.com",
+    "email"=>"john.doe@gmail.com",
+     "first_name"=>"John",
+     "last_name"=>"Doe",
+     "image"=>"https://example.com/photo.jpg"
+    }
+  }.with_indifferent_access
+end
+
+When(/^I log in$/) do
+  find_link('Sign in With Google').click
+end
+
--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -1,36 +1,11 @@
-Given(/^I am not logged in$/) do
-end
-
 Given(/^I visit the '([^"]*)' page$/) do |name|
   visit PondoSpecs::Pages.send("#{ name }_page")
-end
-
-Given(/^I have a google account$/) do
-  OmniAuth.config.mock_auth[:default] = {
-   "provider"=>"google_oauth2",
-   "uid"=>"100299566497892936264",
-   "info"=>
-   {"name"=>"john.doe@gmail.com",
-    "email"=>"john.doe@gmail.com",
-     "first_name"=>"John",
-     "last_name"=>"Doe",
-     "image"=>"https://example.com/photo.jpg"
-    }
-  }.with_indifferent_access
-end
-
-When(/^I log in$/) do
-  find_link('Sign in With Google').click
 end
 
 Then(/^I should see the '([^"]*)' page$/) do |name|
   sleep 0.3
   uri = URI.parse(current_url)
   expect(uri.path).to eql(PondoSpecs::Pages.send("#{ name }_page"))
-end
-
-Given(/^I don't have a google account$/) do
-  OmniAuth.config.mock_auth[:default] = :no_account
 end
 
 Then(/^I should see '(.+)'$/) do |text|
