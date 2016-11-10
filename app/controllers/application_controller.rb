fd9f4154a13732115782efd81ba300d57e405111
Wayne Duran <asartalo@gmail.com>
2016-11-10 17:51:47 +0800

    Force SSL when in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  force_ssl if: :ssl_configured?
+
+  def ssl_configured?
+    Rails.env.production?
+  end
 end
