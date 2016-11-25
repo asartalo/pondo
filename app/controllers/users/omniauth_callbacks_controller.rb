96cd1b4f39c0a8fc6898036da08022c8effa43b5
Wayne Duran <asartalo@gmail.com>
2016-11-26 04:52:10 +0800

    Refactoring OmniauthCallbacksController

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,20 +1,30 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-      # You need to implement the method below in your model (e.g. app/models/user.rb)
-      @user = User.from_omniauth(request.env["omniauth.auth"])
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
 
-      if @user.persisted?
-        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-        sign_in @user
-        redirect_to dashboard_url
-      else
-        session["devise.google_data"] = request.env["omniauth.auth"]
-        redirect_to root_url
-      end
+    if @user.persisted?
+      sign_in_user(@user)
+    else
+      auth_fail
+    end
   end
 
   def failure
     redirect_to root_url
+  end
+
+  protected
+
+  def sign_in_user(user)
+    flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Google"
+    sign_in user
+    redirect_to dashboard_url
+  end
+
+  def auth_fail
+    session["devise.google_data"] = request.env["omniauth.auth"]
+    failure
   end
 end
 
