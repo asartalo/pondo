92fff45aa07305fdea1a7488a0926ed3aa405630
Wayne Duran <asartalo@gmail.com>
2016-12-18 03:05:02 +0800

    Experiment with styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     # Money::Currency.table.values.collect { |c| c[:iso_code]  }
     Money::Currency.all
   end
+
+  def page(identity)
+    content_for(:page) { "#{identity}-page" }
+  end
 end
