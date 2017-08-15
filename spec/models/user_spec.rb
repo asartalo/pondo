9c71529cdeaf638fe897fdd5c0ed4a0b3cb18c1d
Wayne Duran <asartalo@gmail.com>
2017-08-16 02:03:08 +0800

    Annotations to user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string           default(""), not null
+#  name       :string           default(""), not null
+#  provider   :string           default(""), not null
+#  uid        :string           default(""), not null
+#  image      :string           default(""), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
