7b75b39b0fbdb066f61bbf2e13cbf10ebc38eef8
Wayne Duran <asartalo@gmail.com>
2018-09-24 11:11:58 +0800

    CI: Better logging for flaky test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,3 +1,5 @@
+require "pp"
+
 class TestController < ApplicationController
   before_action :test_only
 
@@ -9,13 +11,31 @@ class TestController < ApplicationController
   end
 
   def run
-    puts params[:script_name].to_s
+    puts "\n\n============================================="
+    print_script_or_code
     code = params[:code] || read_file(params.require(:script_name))
-    puts "Running code:", code
-    render json: Kernel.eval(code, binding())
+    pretty_error_wrap do
+      render json: Kernel.eval(code, binding())
+    end
   end
 
   private
+
+  def pretty_error_wrap
+    begin
+      yield
+    rescue Exception => e
+      puts " ERROR!\nParams:"
+      pp params.as_json
+      raise e
+    end
+  end
+
+  def print_script_or_code
+    print params[:script_name] ?
+      "Run Script: #{params[:script_name].to_s}" :
+      "Run Code: #{params[:code]}"
+  end
 
   def read_file(file_name)
     File.read(
--- spec/cypress/app_commands/find_user_subscription.rb
+++ spec/cypress/app_commands/find_user_subscription.rb
@@ -1,3 +1,9 @@
 me = User.find_by(email: params[:email])
-me.subscribed_ledgers.find(params[:ledger])
+ledger = Ledger.find(params[:ledger])
+raise "Did not find any Ledger with id: #{params[:ledger]}" unless ledger
+
+my_ledger = me.subscribed_ledgers.find(params[:ledger])
+raise "User #{me.email} does not have ledger #{params[:ledger]}" unless my_ledger
+
+my_ledger.as_json
 
