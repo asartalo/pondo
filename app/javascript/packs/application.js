c66bd8f579d5f20e0d99636e0c448a4d55f3ed12
Wayne Duran <asartalo@gmail.com>
2018-09-26 10:08:24 +0800

    Use webpack to compile sass instead of sprockets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import '../src/styles/application.scss';
 import $ from 'jquery';
 import '../../assets/javascripts/pu';
 import '../../assets/javascripts/global';
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,19 @@
 const { environment } = require('@rails/webpacker')
+const { resolve } = require("path")
+const eyeglass = require("eyeglass")
 const erb =  require('./loaders/erb')
 const coffee =  require('./loaders/coffee')
 
 environment.loaders.append('coffee', coffee)
 environment.loaders.append('erb', erb)
+
+const sassLoader = environment.loaders.get('sass')
+	.use.find(item => item.loader === 'sass-loader')
+
+sassLoader.options = eyeglass(Object.assign({}, sassLoader.options, {
+	sourceMap: false,
+	sourceComments: true,
+	includePaths: [resolve("app", "javascript", "styles")]
+}))
+
 module.exports = environment
