e0d91d379049b10421a7788d13ee28e3ed861b39
Wayne Duran <asartalo@gmail.com>
2018-09-26 17:05:17 +0800

    CI: Fixing assets precompile error

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,5 +12,10 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile = %w(
-	application.js arrow_downward.svg close.svg logo.svg menu.svg
+	application.js
+	arrow_downward.svg
+	arrow_downward.svg
+	close.svg
+	logo.svg
+	menu.svg
 )
